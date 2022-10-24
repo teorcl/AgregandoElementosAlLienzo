a3b2d83863484064665530444942849cdefae872
Teodoro <teo.calle.lara@gmail.com>
2022-10-24 01:37:09 -0500

    Se realizaron conexiones del controlador a la vista para modificar los labels y de la vista al controlador para que al presionar un botón se hicieran cambios en los lables y en la imagen, se agrego un gestor de estado básico

--- AgregandoElementosAlLienzo/FirstViewController.swift
+++ AgregandoElementosAlLienzo/FirstViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var principalTitleLabel: UILabel!
+    @IBOutlet weak var telephoneNumberLabel: UILabel!
+    @IBOutlet weak var hideButton: UIButton!
+    
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    //Variable de estado
+    var isTelephoneVisible = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        principalTitleLabel.text = "Ya se cargó"
     }
 
 
+    @IBAction func hideButtonPressed(_ sender: UIButton) {
+        if( isTelephoneVisible ){
+            telephoneNumberLabel.text = "*** *** ****"
+            isTelephoneVisible = false
+            hideButton.setTitle("Mostrar", for: .normal)
+            profilePictureImageView.image = UIImage(named: "anonymus")
+        }else{
+            telephoneNumberLabel.text = "+57 3127723718"
+            isTelephoneVisible = true
+            hideButton.setTitle("Ocultar", for: .normal)
+            profilePictureImageView.image = UIImage(named: "foto")
+        }
+    }
+    
 }
 
+  
+  
